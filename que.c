dd9267b76a4afee6af60d8e7c537c1b0f4fe51e9
Isha Chauhan <iishachauhan555@gmail.com>
2025-07-07 08:43:40 +0530

    Create que.c

---
+++ que.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main()
+{
+    char str[100];
+    int i, j, k = 1;
+
+    printf("Enter any string: ");
+    scanf("%s", str);
+
+    //calculating length
+    int len = 0;
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    for (i = 0, j = len - 1; i < j; i++, j--)
+    {
+        if (str[i] != str[j])
+        {
+            k = 0;
+            break;
+        }
+    }
+
+    if (k == 1)
+        printf("The given string is a Palindrome.");
+    else
+        printf("The given string is NOT a Palindrome.");
+}
